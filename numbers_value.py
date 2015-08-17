0a25abf0f0ef7b8bd209d0b448c168f87b208bd9
Grazzi <Grazziella@users.noreply.github.com>
2015-08-17 11:21:57 +0100

    Create numbers_value.py

---
+++ numbers_value.py
@@ -0,0 +1,15 @@
+# enter two numbers, return the biggest one.
+
+def bigger_number(x,y):
+	if x > y:
+		print(x)
+	else:
+		print(y)
+
+# enter two numbers, return the smallest one.
+
+def smaller_number(x,y):
+	if x < y:
+		print(x)
+	else:
+		print(y)
