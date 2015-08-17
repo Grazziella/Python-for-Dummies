a0118c8582a69355cd9d21925633ed15716e6c3a
Grazzi <Grazziella@users.noreply.github.com>
2015-08-17 16:42:53 +0100

    Create prime_number.py

---
+++ prime_number.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def prime_number(x):
+
+	"""Return True if the number is prime, otherwise False!"""
+
+	if x == 2 or x == 3 or x == 5 or x == 7:
+		return 'True'
+
+	elif x % 2 == 0 or x % 3 == 0 or x % 5 == 0 or x % 7 == 0:
+		return 'False'
+
+	else:
+		return 'True'
+
+if __name__ == '__main__':
+	print(prime_number(2))
