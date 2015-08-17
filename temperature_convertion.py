6d2fccdf8e3f48a1500737524997705ef909485d
Grazzi <Grazziella@users.noreply.github.com>
2015-08-17 10:28:49 +0100

    Create temperature_convertion.py

---
+++ temperature_convertion.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+# convertion of temperature in Celsius to Fahrenheit!
+
+def temp_fahrenheit(temperature):
+	return (temperature * 1.8) + 32
+
+# covertion of temperature in Fahrenheit to Celsius!
+
+def temp_celsius(temperature):
+	return (temperature - 32) / 1.8
