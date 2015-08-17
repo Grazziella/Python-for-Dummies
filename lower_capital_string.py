314957229036ea85d9a3578ac4396451591cd670
Grazzi <Grazziella@users.noreply.github.com>
2015-08-17 10:54:41 +0100

    Update lower_capital_string.py

--- lower_capital_string.py
+++ lower_capital_string.py
@@ -1,11 +1,9 @@
 # enter a string and return the string in CAPITAL LETTERS.
 
 def capital_string(string):
-	string = str(string)
 	return string.upper()
 
 # enter a string and return the string in lower case.
 
 def lower_string(string):
-	string = str(string)
 	return string.lower()
