2591ff86e913d9d76f484a4555c89e06131818e7
Grazzi <Grazziella@users.noreply.github.com>
2015-08-17 14:48:54 +0100

    Create even_odd.py

---
+++ even_odd.py
@@ -0,0 +1,12 @@
+def even_odd(x):
+
+    """Return if the number is odd or even."""
+    
+    if x % 2 == 0:
+    	print('Even')
+    else:
+    	print('Odd')
+
+if __name__ == '__main__':
+    even_odd(10)
+    even_odd(1)
