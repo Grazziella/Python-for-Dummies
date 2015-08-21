7ad1ed59ea979a6e727d58a86932c38806e36251
Grazzi <Grazziella@users.noreply.github.com>
2015-08-21 11:13:48 +0100

    Update reverse_str.py

--- reverse_str.py
+++ reverse_str.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# using for loop:
+
 def reverse_str(name):
 
 	""" Return the reverse string. """
@@ -14,3 +16,22 @@ def reverse_str(name):
 
 if __name__ == '__main__':
 	print(reverse_str('Arara'))
+
+# using while loop:
+
+def reverse_str(string):
+
+	""" Return the reverse string. """	
+	
+	reverse = ''
+	index = 0
+	
+	while index < len(string):
+
+		index = index + 1
+		reverse += string[len(string) - index]
+		
+	print(reverse)
+
+if __name__ == '__main__':
+	reverse_str('Banana')
